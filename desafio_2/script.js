dc44a8691da64fa40cfca14d553b04fd7d3e4d94
glauberbandeira <glauberbandeira_@hotmail.com>
2022-01-09 01:46:27 -0300

    Adicionando 3 desafios finalizados

---
+++ desafio_2/script.js
@@ -0,0 +1,13 @@
+// Animação
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+});
+
+ScrollReveal().reveal('.banner-section', { delay: 200 });
+ScrollReveal().reveal('.wallpaper', { delay: 400 });
+ScrollReveal().reveal('.title', { delay: 500 });
+ScrollReveal().reveal('.subtitle', { delay: 600 });
+ScrollReveal().reveal('.card-shop', { delay: 700 });
